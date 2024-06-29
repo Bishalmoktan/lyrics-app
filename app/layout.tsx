a511ff56a7b6cae2c8396d63cf4df13ca79afbea
Bishal Moktan <bishalmoktan270@gmail.com>
2024-06-29 13:08:32 +0530

    Delete action updated, added top loading bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { AppContextProvider } from "@/components/providers/app-context-provider";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import NextTopLoader from "nextjs-toploader"
 
 const font = Space_Grotesk({ subsets: ["latin"] });
 
@@ -43,6 +44,21 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={font.className}>
+      <NextTopLoader
+  color="#2299DD"
+  initialPosition={0.08}
+  crawlSpeed={200}
+  height={3}
+  crawl={true}
+  showSpinner={true}
+  easing="ease"
+  speed={200}
+  shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+  template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+  zIndex={1600}
+  showAtBottom={false}
+/>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
