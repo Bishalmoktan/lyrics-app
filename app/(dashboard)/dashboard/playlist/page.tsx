d44c04e907ffc0b8d3cc1379cafb0f572059b5a9
Bishal Moktan <bishalmoktan270@gmail.com>
2024-11-01 23:08:04 +0530

    fix: type correct for loading state

--- app/(dashboard)/dashboard/playlist/page.tsx
+++ app/(dashboard)/dashboard/playlist/page.tsx
@@ -43,7 +43,6 @@ export default function PlaylistPage() {
       )}
       {playlists === null && (
         <section>
-          <h2 className="text-2xl font-semibold mb-4">Artists</h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {Array(6)
               .fill(0)
